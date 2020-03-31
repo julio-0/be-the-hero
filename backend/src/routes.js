1f3d062c3adea2046ffbd231ced017a956e14c6a
julio-0 <julio-0@users.noreply.github.com>
2020-03-31 08:58:11 -0300

    Backend validation examples and some automated tests added

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
+
 const ongcontroller = require('./controllers/ongcontroller');
 const incidentcontroller = require('./controllers/incidentcontroller');
 const profilecontroller = require('./controllers/profilecontroller');
@@ -9,12 +11,44 @@ const routes = express.Router();
 routes.post('/sessions', sessioncontroller.create);
 
 routes.get('/ongs', ongcontroller.index);
-routes.post('/ongs', ongcontroller.create);
 
-routes.get('/incidents', incidentcontroller.index);
-routes.post('/incidents', incidentcontroller.create);
-routes.delete('/incidents/:id', incidentcontroller.delete);
+routes.post('/ongs', celebrate({
+    [Segments.BODY] : Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(13),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), ongcontroller.create);
 
-routes.get('/profile', profilecontroller.index);
+routes.get('/incidents', celebrate({
+    [Segments.QUERY] : Joi.object().keys({
+        page: Joi.number(),
+    })
+}),incidentcontroller.index);
+
+routes.post('/incidents', celebrate({    
+    [Segments.HEADERS] : Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+    [Segments.BODY] : Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number(),
+    })    
+}), incidentcontroller.create);
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS] : Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), incidentcontroller.delete);
+
+routes.get('/profile', celebrate({    
+    [Segments.HEADERS] : Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), profilecontroller.index);
 
 module.exports = routes;
